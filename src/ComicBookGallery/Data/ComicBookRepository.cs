11aa1a7fe9361c96a75b20d84930d83a2a6c2fa0
akhil <akhilkumara.94@gmail.com>
2017-08-17 00:32:18 +0530

    Added comic books list view

--- src/ComicBookGallery/Data/ComicBookRepository.cs
+++ src/ComicBookGallery/Data/ComicBookRepository.cs
@@ -57,6 +57,11 @@ namespace ComicBookGallery.Data
                 Id = 3
             }
         };
+        public ComicBook[] GetComicBooks()
+        {
+            return _comicBooks;
+        }
+
         public ComicBook GetComicBook(int id)
         {
             ComicBook comicBookToReturn = null;
